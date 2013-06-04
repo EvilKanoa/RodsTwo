472986e3949904f8cbd2f404ea54db723ea9a398
EvilKanoa <kanoa@kanoa.ca>
2013-06-04 10:13:50 -0300

    Added the ender rod

---
+++ src/ca/kanoa/RodsTwo/Rods/Ender.java
@@ -0,0 +1,24 @@
+package ca.kanoa.RodsTwo.Rods;
+
+import ca.kanoa.RodsTwo.Objects.ConfigOptions;
+import ca.kanoa.RodsTwo.Objects.Rod;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EnderPearl;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.Plugin;
+
+public class Ender extends Rod {
+
+	public Ender(Plugin plugin) throws Exception {
+	    super("Ender", 1, 280, new ConfigOptions(new String[]{}, new Object[]{}), 500, plugin);
+	    setRecipe(new ShapedRecipe(super.getItem()).shape("EBE").setIngredient('E', Material.ENDER_PEARL).setIngredient('B', Material.STICK));
+	}
+	
+	@Override
+	public boolean run(Player player, ConfigurationSection config) {
+	    player.launchProjectile(EnderPearl.class);
+		return true; 
+	}
+}
