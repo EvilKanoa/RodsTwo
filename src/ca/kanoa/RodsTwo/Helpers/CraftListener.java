7aa8760823ee0d8a9eb8586218eff7a427346b42
EvilKanoa <kanoa@kanoa.ca>
2013-06-06 05:51:35 -0700

    Added permission based crafting, use lr.craft.all or lr.craft.rodName

--- src/ca/kanoa/RodsTwo/Helpers/CraftListener.java
+++ src/ca/kanoa/RodsTwo/Helpers/CraftListener.java
@@ -1,13 +1,29 @@
 package ca.kanoa.RodsTwo.Helpers;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+
+import ca.kanoa.RodsTwo.RodsTwo;
+import ca.kanoa.RodsTwo.Objects.Rod;
 
 public class CraftListener implements Listener {
 
 	@EventHandler
 	public void onRodCraft(CraftItemEvent event){
+		
+		Player player = (Player) event.getViewers().get(0);
+		if (player.hasPermission("lr.craft.all"))
+			return;
+		
+		for (Rod rod : RodsTwo.rods) 
+			if (event.getRecipe().getResult().getItemMeta().getLore().get(0).equals(rod.getRecipe().getResult().getItemMeta().getLore().get(0))) 
+				if (!player.hasPermission(rod.getCraftPermission())) {
+					event.setCancelled(true);
+					player.sendMessage(ChatColor.RED + "You don't have permissions to craft that!");
+				}
 		
 	}
 	
