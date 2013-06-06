8edff968dff5b7187d5863c2ee5097a349d525ce
EvilKanoa <kanoa@kanoa.ca>
2013-06-06 10:44:40 -0300

    Added fall damage protection to the Jump rod

--- src/ca/kanoa/RodsTwo/Rods/Jump.java
+++ src/ca/kanoa/RodsTwo/Rods/Jump.java
@@ -1,17 +1,29 @@
 package ca.kanoa.RodsTwo.Rods;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.Plugin;
 
+import ca.kanoa.RodsTwo.RodsTwo;
 import ca.kanoa.RodsTwo.Objects.ConfigOptions;
 import ca.kanoa.RodsTwo.Objects.Rod;
 
-public class Jump extends Rod {
+public class Jump extends Rod implements Listener {
 
+	private Set<String> jumpees;
+	
 	public Jump(Plugin plugin) throws Exception {
 	    super("Jump", 1, 280, new ConfigOptions(new String[]{"power"}, new Object[]{5.0d}), 2000);
 	    setRecipe(new ShapedRecipe(super.getItem()).shape(" B ", " I ").setIngredient('I', Material.IRON_BOOTS).setIngredient('B', Material.STICK));
@@ -21,6 +33,33 @@ public class Jump extends Rod {
 	public boolean run(Player player, ConfigurationSection config) {
 		player.setVelocity(player.getVelocity().setY(config.getDouble("power") / 4));
 		player.playSound(player.getLocation(), Sound.EXPLODE, 1, 1);
+		final String playerName = player.getName();
+		jumpees.add(playerName);
+		Bukkit.getScheduler().scheduleSyncDelayedTask(RodsTwo.plugin, new Runnable(){
+			@Override
+			public void run() {
+				if (jumpees.contains(playerName))
+					jumpees.remove(playerName);
+			}}, 140);
 	    return true; 
+	}
+	
+	@Override
+	public boolean enable(Server serv) {
+		jumpees = new HashSet<String>();
+		Bukkit.getPluginManager().registerEvents(this, RodsTwo.plugin);
+		return true;
+	}
+	
+	@EventHandler
+	public void onPlayerFall(EntityDamageEvent event) {
+		if (event.getCause() == DamageCause.FALL && event.getEntity() instanceof Player) {
+			Player p = (Player) event.getEntity();
+			if (jumpees.contains(p.getName())) {
+				event.setCancelled(true);
+				jumpees.remove(p.getName());
+				p.playSound(p.getLocation(), Sound.FALL_BIG, 1, 1);
+			}
+		}
 	}
 }
