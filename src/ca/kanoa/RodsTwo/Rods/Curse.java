acedaea975d59747ac1d0434527fcf744a89a1a4
EvilKanoa <kanoa@kanoa.ca>
2013-06-09 02:07:05 -0300

    Kills with curse rod now counts for the player

--- src/ca/kanoa/RodsTwo/Rods/Curse.java
+++ src/ca/kanoa/RodsTwo/Rods/Curse.java
@@ -1,10 +1,14 @@
 package ca.kanoa.RodsTwo.Rods;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
+import ca.kanoa.RodsTwo.RodsTwo;
 import ca.kanoa.RodsTwo.Objects.ConfigOptions;
 import ca.kanoa.RodsTwo.Objects.Rod;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.Server;
@@ -13,15 +17,21 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.BlockIterator;
 
-public class Curse extends Rod {
+public class Curse extends Rod implements Listener {
 
 	private Random rand;
+	private Set<AffectedPlayer> affected;
 
 	public Curse(Plugin plugin) throws Exception {
 		super("Curse", 1, 280, new ConfigOptions(new String[]{"random_potion_length", "random_potion_level"}, new Object[]{30, 2}), 2000);
@@ -30,34 +40,62 @@ public class Curse extends Rod {
 
 	@Override
 	public boolean run(Player player, ConfigurationSection config) {
-		
+
 		Entity target = getTarget(player);
 		if (target == null || !(target instanceof LivingEntity))
 			return false;
 
 		PotionEffectType type;
-		switch (rand.nextInt(8) + 1) {
+		switch (rand.nextInt(7) + 1) {
 		case 1:   type = PotionEffectType.BLINDNESS;    break;
 		case 2:   type = PotionEffectType.CONFUSION;    break;
 		case 3:   type = PotionEffectType.HARM;         break;
-		case 4:   type = PotionEffectType.HUNGER;       break;
-		case 5:   type = PotionEffectType.POISON;       break;
-		case 6:   type = PotionEffectType.WITHER;       break;
-		case 7:   type = PotionEffectType.SLOW;         break;
-		case 8:   type = PotionEffectType.WEAKNESS;     break;
+		case 4:   type = PotionEffectType.POISON;       break;
+		case 5:   type = PotionEffectType.WITHER;       break;
+		case 6:   type = PotionEffectType.SLOW;         break;
+		case 7:   type = PotionEffectType.WEAKNESS;     break;
 		default:  type = PotionEffectType.HEAL;
 		}
 		PotionEffect potion = new PotionEffect(type, config.getInt("random_potion_length") * 20, config.getInt("random_potion_level") - 1);
-		
+
 		((LivingEntity)target).addPotionEffect(potion);
 		target.getWorld().playEffect(target.getLocation(), Effect.POTION_BREAK, 4);
+
+		if (target instanceof Player) {
+			final AffectedPlayer afft = new AffectedPlayer(player, (Player) target, potion);
+			affected.add(afft);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(RodsTwo.plugin, new Runnable(){
+				public void run() {
+					affected.remove(afft);
+				}}, (config.getInt("random_potion_length") * 20) + 30);
+		}
+
 		return true;
 	}
 
 	@Override
 	public boolean enable(Server serv) {
 		rand = new Random(serv.getSpawnRadius() * serv.getMaxPlayers() * 10);
+		Bukkit.getPluginManager().registerEvents(this, RodsTwo.plugin);
+		affected = new HashSet<AffectedPlayer>();
 		return true;
+	}
+
+	@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+	public void onPlayerDeath(EntityDamageEvent event) {
+		if ((event.getCause() == DamageCause.MAGIC || 
+				event.getCause() == DamageCause.WITHER) && 
+				event.getEntity() instanceof Player &&
+				((Player)event.getEntity()).getHealth() - event.getDamage() <= 0) {
+			Player player = (Player) event.getEntity();
+			for (AffectedPlayer ap : affected) {
+				if (ap.getAffectee() == player && player.hasPotionEffect(ap.getEffect().getType())) {
+					event.setCancelled(true);
+					player.damage(event.getDamage(), ap.getAffecter());
+					affected.remove(player.getName());
+				}
+			}
+		}
 	}
 
 	private Entity getTarget(Player player) {
@@ -79,5 +117,31 @@ public class Curse extends Rod {
 			}
 		}
 		return target;
+	}
+
+	private class AffectedPlayer {
+
+		private final Player affecter;
+		private final Player affectee;
+		private final PotionEffect effect;
+
+		public AffectedPlayer(Player affecter, Player affectee, PotionEffect effect) {
+			this.affecter = affecter;
+			this.affectee = affectee;
+			this.effect = effect;
+		}
+
+		public PotionEffect getEffect() {
+			return effect;
+		}
+
+		public Player getAffecter() {
+			return affecter;
+		}
+
+		public Player getAffectee() {
+			return affectee;
+		}
+
 	}
 }
