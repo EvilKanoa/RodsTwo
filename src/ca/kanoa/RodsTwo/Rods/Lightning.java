f4dea6623c14f0a521ea44aac427ab52fd8764df
EvilKanoa <kanoa@kanoa.ca>
2013-06-09 01:09:11 -0300

    When player dies from Lightning rod, it credits the attacker

--- src/ca/kanoa/RodsTwo/Rods/Lightning.java
+++ src/ca/kanoa/RodsTwo/Rods/Lightning.java
@@ -1,15 +1,28 @@
 package ca.kanoa.RodsTwo.Rods;
 
+import java.util.HashMap;
+
+import ca.kanoa.RodsTwo.RodsTwo;
 import ca.kanoa.RodsTwo.Objects.ConfigOptions;
 import ca.kanoa.RodsTwo.Objects.Rod;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LightningStrike;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.Plugin;
 
-public class Lightning extends Rod {
+public class Lightning extends Rod implements Listener {
 
+	private HashMap<Integer, String> strikeID;
+	
     public Lightning(Plugin plugin) throws Exception {
         super("Lightning", 1, 280,  new ConfigOptions(new String[]{"max_distance"}, new Object[]{50}), 1500, plugin);
 
@@ -19,7 +32,32 @@ public class Lightning extends Rod {
 
     @Override
     public boolean run(Player player, ConfigurationSection config){
-        player.getWorld().strikeLightning(player.getTargetBlock(null, config.getInt("max_distence")).getLocation());
+    	final int id = player.getWorld().strikeLightning(player.getTargetBlock(null, config.getInt("max_distence")).getLocation()).getEntityId();
+        strikeID.put(id, player.getName());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(RodsTwo.plugin, new Runnable(){
+			public void run() {
+				strikeID.remove(id);
+			}}, 100);
         return true;
     }
+    
+    @Override
+    public boolean enable(Server serv) {
+    	strikeID = new HashMap<Integer, String>();
+    	Bukkit.getPluginManager().registerEvents(this, RodsTwo.plugin);
+    	return true;
+    }
+    
+    @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+    public void onPlayerDeath(EntityDamageByEntityEvent event) {
+    	if (event.getEntity() instanceof Player && 
+    			event.getDamager() instanceof LightningStrike &&
+    			strikeID.containsKey(event.getDamager().getEntityId()) &&
+    			((Player)event.getEntity()).getHealth() - event.getDamage() <= 0 &&
+    			Bukkit.getServer().getPlayer(strikeID.get(event.getDamager().getEntityId())) != null) {
+    		event.setCancelled(true);
+    		((Player)event.getEntity()).damage(event.getDamage(), Bukkit.getPlayer(strikeID.get(event.getDamager().getEntityId())));
+    	}
+    }
+    
 }
