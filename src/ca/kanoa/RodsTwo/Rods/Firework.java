a6fd57f4bfa6f612a60069eb1ff1c25eb9d2b8f2
EvilKanoa <kanoa@kanoa.ca>
2013-06-16 00:41:39 -0700

    Added a new checking method for firework rod, ignores long grass and water now

--- src/ca/kanoa/RodsTwo/Rods/Firework.java
+++ src/ca/kanoa/RodsTwo/Rods/Firework.java
@@ -1,7 +1,9 @@
 package ca.kanoa.RodsTwo.Rods;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import ca.kanoa.RodsTwo.RodsTwo;
 import ca.kanoa.RodsTwo.Objects.ConfigOptions;
 import ca.kanoa.RodsTwo.Objects.Rod;
@@ -25,7 +27,7 @@ import org.bukkit.plugin.Plugin;
 public class Firework extends Rod {
 
 	private FireworkEffectPlayer fePlayer;
-	
+
 	public Firework(Plugin plugin) throws Exception {
 		super("Firework", 1, 280, new ConfigOptions(new String[]{"radius", "damage"}, new Object[]{4, 10}), 1000);
 		setRecipe(new ShapedRecipe(super.getItem()).shape("DDD", "DBD", "DDD").setIngredient('D', Material.SPONGE).setIngredient('B', Material.STICK));
@@ -61,11 +63,13 @@ public class Firework extends Rod {
 		fePlayer = new FireworkEffectPlayer();
 		return true;
 	}
-	
+
 	class FireworkHitBlock implements Runnable {
 
 		private org.bukkit.entity.Firework firework;
 		private FireworkRunnable whenHitsBlock;
+
+		public final List<Material> trans = Arrays.asList(new Material[]{Material.AIR, Material.LONG_GRASS, Material.WATER});
 
 		public FireworkHitBlock (org.bukkit.entity.Firework firework, FireworkRunnable whenHitsBlock) {
 			this.firework = firework;
@@ -79,11 +83,11 @@ public class Firework extends Rod {
 				firework.remove();
 			else if (loc.getType() != Material.AIR ||
 					(Double.parseDouble((firework.getLocation().getY() + "").substring(2)) < 0.6f &&
-							loc.getRelative(0, -1, 0).getType() != Material.AIR) ||
-					loc.getRelative(1, 0, 0).getType() != Material.AIR ||
-					loc.getRelative(0, 0, 1).getType() != Material.AIR ||
-					loc.getRelative(-1, 0, 0).getType() != Material.AIR ||
-					loc.getRelative(0, 0, -1).getType() != Material.AIR) {
+							!trans.contains(loc.getRelative(0, -1, 0).getType())) ||
+							!trans.contains(loc.getRelative(1, 0, 0).getType()) ||
+							!trans.contains(loc.getRelative(0, 0, 1).getType()) ||
+							!trans.contains(loc.getRelative(-1, 0, 0).getType()) ||
+							!trans.contains(loc.getRelative(0, 0, -1).getType())) {
 				firework.remove();
 				for (FireworkEffect fe : firework.getFireworkMeta().getEffects())
 					try {
@@ -98,11 +102,11 @@ public class Firework extends Rod {
 		}
 
 	}
-	
+
 	interface FireworkRunnable {
 		public void run(Location loc, org.bukkit.entity.Firework firework);
 	}
-	
+
 	/**
 	 * FireworkEffectPlayer v1.0
 	 * 
@@ -116,92 +120,92 @@ public class Firework extends Rod {
 	 * @author codename_B
 	 */
 	public class FireworkEffectPlayer {
-	    
-	    /*
-	     * Example use:
-	     * 
-	     * public class FireWorkPlugin implements Listener {
-	     * 
-	     * FireworkEffectPlayer fplayer = new FireworkEffectPlayer();
-	     * 
-	     * @EventHandler
-	     * public void onPlayerLogin(PlayerLoginEvent event) {
-	     *   fplayer.playFirework(event.getPlayer().getWorld(), event.getPlayer.getLocation(), Util.getRandomFireworkEffect());
-	     * }
-	     * 
-	     * }
-	     */
-	    
-	    // internal references, performance improvements
-	    private Method world_getHandle = null;
-	    private Method nms_world_broadcastEntityEffect = null;
-	    private Method firework_getHandle = null;
-	    
-	    /**
-	     * Play a pretty firework at the location with the FireworkEffect when called
-	     * @param world
-	     * @param loc
-	     * @param fe
-	     * @throws Exception
-	     */
-	    public void playFirework(World world, Location loc, FireworkEffect fe) throws Exception {
-	        // Bukkity load (CraftFirework)
-	    	org.bukkit.entity.Firework fw = (org.bukkit.entity.Firework) world.spawn(loc, org.bukkit.entity.Firework.class);
-	        // the net.minecraft.server.World
-	        Object nms_world = null;
-	        Object nms_firework = null;
-	        /*
-	         * The reflection part, this gives us access to funky ways of messing around with things
-	         */
-	        if(world_getHandle == null) {
-	            // get the methods of the craftbukkit objects
-	            world_getHandle = getMethod(world.getClass(), "getHandle");
-	            firework_getHandle = getMethod(fw.getClass(), "getHandle");
-	        }
-	        // invoke with no arguments
-	        nms_world = world_getHandle.invoke(world, (Object[]) null);
-	        nms_firework = firework_getHandle.invoke(fw, (Object[]) null);
-	        // null checks are fast, so having this seperate is ok
-	        if(nms_world_broadcastEntityEffect == null) {
-	            // get the method of the nms_world
-	            nms_world_broadcastEntityEffect = getMethod(nms_world.getClass(), "broadcastEntityEffect");
-	        }
-	        /*
-	         * Now we mess with the metadata, allowing nice clean spawning of a pretty firework (look, pretty lights!)
-	         */
-	        // metadata load
-	        FireworkMeta data = (FireworkMeta) fw.getFireworkMeta();
-	        // clear existing
-	        data.clearEffects();
-	        // power of one
-	        data.setPower(1);
-	        // add the effect
-	        data.addEffect(fe);
-	        // set the meta
-	        fw.setFireworkMeta(data);
-	        /*
-	         * Finally, we broadcast the entity effect then kill our fireworks object
-	         */
-	        // invoke with arguments
-	        nms_world_broadcastEntityEffect.invoke(nms_world, new Object[] {nms_firework, (byte) 17});
-	        // remove from the game
-	        fw.remove();
-	    }
-	    
-	    /**
-	     * Internal method, used as shorthand to grab our method in a nice friendly manner
-	     * @param cl
-	     * @param method
-	     * @return Method (or null)
-	     */
-	    private Method getMethod(Class<?> cl, String method) {
-	        for(Method m : cl.getMethods()) {
-	            if(m.getName().equals(method)) {
-	                return m;
-	            }
-	        }
-	        return null;
-	    }
+
+		/*
+		 * Example use:
+		 * 
+		 * public class FireWorkPlugin implements Listener {
+		 * 
+		 * FireworkEffectPlayer fplayer = new FireworkEffectPlayer();
+		 * 
+		 * @EventHandler
+		 * public void onPlayerLogin(PlayerLoginEvent event) {
+		 *   fplayer.playFirework(event.getPlayer().getWorld(), event.getPlayer.getLocation(), Util.getRandomFireworkEffect());
+		 * }
+		 * 
+		 * }
+		 */
+
+		// internal references, performance improvements
+		private Method world_getHandle = null;
+		private Method nms_world_broadcastEntityEffect = null;
+		private Method firework_getHandle = null;
+
+		/**
+		 * Play a pretty firework at the location with the FireworkEffect when called
+		 * @param world
+		 * @param loc
+		 * @param fe
+		 * @throws Exception
+		 */
+		public void playFirework(World world, Location loc, FireworkEffect fe) throws Exception {
+			// Bukkity load (CraftFirework)
+			org.bukkit.entity.Firework fw = (org.bukkit.entity.Firework) world.spawn(loc, org.bukkit.entity.Firework.class);
+			// the net.minecraft.server.World
+			Object nms_world = null;
+			Object nms_firework = null;
+			/*
+			 * The reflection part, this gives us access to funky ways of messing around with things
+			 */
+			if(world_getHandle == null) {
+				// get the methods of the craftbukkit objects
+				world_getHandle = getMethod(world.getClass(), "getHandle");
+				firework_getHandle = getMethod(fw.getClass(), "getHandle");
+			}
+			// invoke with no arguments
+			nms_world = world_getHandle.invoke(world, (Object[]) null);
+			nms_firework = firework_getHandle.invoke(fw, (Object[]) null);
+			// null checks are fast, so having this seperate is ok
+			if(nms_world_broadcastEntityEffect == null) {
+				// get the method of the nms_world
+				nms_world_broadcastEntityEffect = getMethod(nms_world.getClass(), "broadcastEntityEffect");
+			}
+			/*
+			 * Now we mess with the metadata, allowing nice clean spawning of a pretty firework (look, pretty lights!)
+			 */
+			// metadata load
+			FireworkMeta data = (FireworkMeta) fw.getFireworkMeta();
+			// clear existing
+			data.clearEffects();
+			// power of one
+			data.setPower(1);
+			// add the effect
+			data.addEffect(fe);
+			// set the meta
+			fw.setFireworkMeta(data);
+			/*
+			 * Finally, we broadcast the entity effect then kill our fireworks object
+			 */
+			// invoke with arguments
+			nms_world_broadcastEntityEffect.invoke(nms_world, new Object[] {nms_firework, (byte) 17});
+			// remove from the game
+			fw.remove();
+		}
+
+		/**
+		 * Internal method, used as shorthand to grab our method in a nice friendly manner
+		 * @param cl
+		 * @param method
+		 * @return Method (or null)
+		 */
+		private Method getMethod(Class<?> cl, String method) {
+			for(Method m : cl.getMethods()) {
+				if(m.getName().equals(method)) {
+					return m;
+				}
+			}
+			return null;
+		}
 
 	}
 
