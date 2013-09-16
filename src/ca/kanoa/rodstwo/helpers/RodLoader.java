480d861411e4e4f1a0a91a4610fceebc51d19908
unknown <halek3449@KCI-IC0027594.wrdsb.ca>
2013-09-16 13:31:58 -0400

    Added error handling to RodLoader

--- src/ca/kanoa/rodstwo/helpers/RodLoader.java
+++ src/ca/kanoa/rodstwo/helpers/RodLoader.java
@@ -1,6 +1,8 @@
 package ca.kanoa.rodstwo.helpers;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Enumeration;
@@ -15,37 +17,70 @@ import ca.kanoa.rodstwo.objects.Rod;
 
 public class RodLoader {
 
-	public static Set<Rod> getRods(File rodDirectory) throws Exception {
+	public static Set<Rod> getRods(File rodDirectory) {
 		rodDirectory.mkdir();
 		URLClassLoader classLoader;
 		Set<Rod> rods = new HashSet<Rod>();
+		JarFile jar = null;
+		Class<?> loadedClass = null;
+		JarEntry entry = null;
 
 		for (File f : rodDirectory.listFiles()) {
-			if (f.isDirectory() || !f.getName().endsWith(".jar"))
-				continue;
-			
-			JarFile jar = new JarFile(f);
-			Enumeration<JarEntry> e = jar.entries();
-			classLoader = URLClassLoader.newInstance(new URL[]{new URL("jar:file:" + f.getAbsolutePath() + "!/")}, RodsTwo.plugin.getClass().getClassLoader());
 
-			while (e.hasMoreElements()) {
-				JarEntry j = (JarEntry) e.nextElement();
-				if(j.isDirectory() || !j.getName().endsWith(".class")){
+			try {
+				if (f.isDirectory() || !f.getName().endsWith(".jar"))
 					continue;
+
+				jar = new JarFile(f);
+				Enumeration<JarEntry> e = jar.entries();
+				classLoader = URLClassLoader.newInstance(new URL[]{new URL("jar:file:" + f.getAbsolutePath() + "!/")}, RodsTwo.plugin.getClass().getClassLoader());
+
+				while (e.hasMoreElements()) {
+					entry = (JarEntry) e.nextElement();
+					if(entry.isDirectory() || !entry.getName().endsWith(".class"))
+						continue;
+					
+					try {
+						loadedClass = classLoader.loadClass(entry.getName().substring(0, entry.getName().length() - 6));
+						CustomRod a = loadedClass.getAnnotation(CustomRod.class);
+						
+						if (a == null)
+							continue;
+						if (a.minimumVersion() > RodsTwo.getVersion())
+							continue;
+
+						rods.add((Rod) loadedClass.getConstructor().newInstance());
+						
+					} catch (ClassNotFoundException e1) {
+						e1.printStackTrace();
+						print("Error while loading class: " + entry.getName() + " (Is it up to date?)");
+					} catch (InstantiationException | IllegalAccessException 
+							| IllegalArgumentException| InvocationTargetException 
+							| NoSuchMethodException | SecurityException e1) {
+						e1.printStackTrace();
+						print("Error while initializing object: " + loadedClass.getName() + " (Is it compiled properly?)");
+					} finally {
+						loadedClass = null;
+					}
+
 				}
-				Class<?> c = classLoader.loadClass(j.getName().substring(0, j.getName().length() - 6));
-				CustomRod a = c.getAnnotation(CustomRod.class);
-				if (a == null)
-					continue;
-				if (a.minimumVersion() > RodsTwo.getVersion())
-					continue;
-				rods.add((Rod) c.getConstructor().newInstance());
+			} catch (IOException e) {
+				e.printStackTrace();
+				print("Error while loading jar file: " + f.getName() + " (Is it corrupt?)");
+			}  finally {
+				try {
+					jar.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-			
-			jar.close();
-			
+
 		}
 		return rods;
+	}
+
+	private static void print(String str) {
+		System.out.println(str);
 	}
 
 }
